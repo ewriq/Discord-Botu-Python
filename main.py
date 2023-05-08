e7c0c7d0268299bc705947db1557da23900b4918
Ewriq <102306313+ewriq@users.noreply.github.com>
2023-05-08 15:39:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import os # os modülünü yükler
 from flask import Flask
 from discord.ext import commands 
 from dotenv import load_dotenv
-
 load_dotenv() # .env yi yükler
-
 app = Flask(__name__)
-
 token = os.environ.get("token") # .env dosyasından token değerini alır
-
 bot = commands.Bot(command_prefix="!")
+
+
+def load_extensions():
+    for filename in os.listdir('./komutlar'): #komutlar klasörüne komutları ekleyebilmeniz için düzeltir.
+        if filename.endswith('.py'):
+            bot.load_extension(f'komutlar.{filename[:-3]}')
+
+load_extensions()
 
 @bot.event
 async def on_ready():
